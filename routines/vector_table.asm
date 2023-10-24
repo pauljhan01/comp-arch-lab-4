a88a639ffdbd0b4ed56ae93d5f606409aac945ce
Paul J. Han <pauljhan@utexas.edu>
2023-10-24 18:15:11 -0500

    completed vector table, 20 bytes, user program

---
+++ routines/vector_table.asm
@@ -0,0 +1,6 @@
+.ORIG x0200
+.FILL x1200 ;ISR, vector x01
+.FILL x1600 ;Protection exception handler, vector x02
+.FILL x1A00 ;Unaligned access exception handler, vector x03
+.FILL x1C00 ;Unknown opcode exception handler, vector x04
+.END
